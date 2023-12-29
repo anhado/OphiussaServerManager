85030455f01d8d851439a41e44224d6c13bcff73
Tiag0ss <tiago.homem83@gmail.com>
2023-12-29 18:15:18 +0000

    added image to listbox

--- OphiussaServerManager/Components/exListBox.cs
+++ OphiussaServerManager/Components/exListBox.cs
@@ -3,11 +3,12 @@ using System.Windows.Forms;
 
 namespace OphiussaServerManager {
     public class ExListBoxItem {
-        public ExListBoxItem(string id, string title, string details, object pObject) {
+        public ExListBoxItem(string id, string title, string details, object pObject, Image image = null) {
             Id               = id;
             Title            = title;
             Details          = details;
             AssociatedObject = pObject;
+            ItemImage        = image;
         }
 
         public string Id { get; set; }
@@ -18,9 +19,14 @@ namespace OphiussaServerManager {
 
         public object AssociatedObject { get; set; }
 
+        public Image ItemImage { get; set; }
 
-        public void DrawItem(DrawItemEventArgs e,         Padding margin,
-                             Font              titleFont, Font    detailsFont, StringFormat aligment) {
+        public void DrawItem(DrawItemEventArgs e,
+                             Padding           margin,
+                             Font              titleFont,
+                             Font              detailsFont,
+                             StringFormat      aligment,
+                             Size?             imageSize = null) {
             // if selected, mark the background differently
             if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
                 e.Graphics.FillRectangle(Brushes.CornflowerBlue, e.Bounds);
@@ -30,17 +36,26 @@ namespace OphiussaServerManager {
             // draw some item separator
             e.Graphics.DrawLine(Pens.DarkGray, e.Bounds.X, e.Bounds.Y, e.Bounds.X + e.Bounds.Width, e.Bounds.Y);
 
+            // draw item image
+            int imgWidth  = 0;
+            int imgHeight = 0;
+            if (ItemImage != null) {
+                imgWidth  = imageSize.Value.Width;
+                imgHeight = imageSize.Value.Height;
+                e.Graphics.DrawImage(this.ItemImage, e.Bounds.X + margin.Left, e.Bounds.Y + margin.Top, imgWidth, imgHeight);
+            }
+ 
             // calculate bounds for title text drawing
-            var titleBounds = new Rectangle(e.Bounds.X                 + margin.Horizontal,
-                                            e.Bounds.Y                 + margin.Top,
-                                            e.Bounds.Width             - margin.Right - margin.Horizontal,
-                                            (int)titleFont.GetHeight() + 2);
+            Rectangle titleBounds = new Rectangle(e.Bounds.X                 + margin.Horizontal + imgWidth,
+                                                  e.Bounds.Y                 + margin.Top,
+                                                  e.Bounds.Width             - margin.Right - imgWidth - margin.Horizontal,
+                                                  (int)titleFont.GetHeight() + 2);
 
             // calculate bounds for details text drawing
-            var detailBounds = new Rectangle(e.Bounds.X      + margin.Horizontal,
-                                             e.Bounds.Y      + (int)titleFont.GetHeight() + 2 + margin.Vertical + margin.Top,
-                                             e.Bounds.Width  - margin.Right               - margin.Horizontal,
-                                             e.Bounds.Height - margin.Bottom              - (int)titleFont.GetHeight() - 2 - margin.Vertical - margin.Top);
+            Rectangle detailBounds = new Rectangle(e.Bounds.X      + margin.Horizontal          + imgWidth,
+                                                   e.Bounds.Y      + (int)titleFont.GetHeight() + 2                          + margin.Vertical + margin.Top,
+                                                   e.Bounds.Width  - margin.Right               - imgWidth                   - margin.Horizontal,
+                                                   e.Bounds.Height - margin.Bottom              - (int)titleFont.GetHeight() - 2 - margin.Vertical - margin.Top);
 
             // draw the text within the bounds
             e.Graphics.DrawString(Title,   titleFont,   Brushes.Black,    titleBounds,  aligment);
@@ -53,14 +68,20 @@ namespace OphiussaServerManager {
 
     public partial class ExListBox : ListBox {
         private readonly Font _detailsFont;
+        private readonly Size _imageSize;
 
         private readonly StringFormat _fmt;
         private readonly Font         _titleFont;
 
-        public ExListBox(Font            titleFont, Font            detailsFont,
-                         StringAlignment aligment,  StringAlignment lineAligment) {
+        public ExListBox(Font            titleFont,
+                         Font            detailsFont,
+                         Size            imageSize,
+                         StringAlignment aligment,
+                         StringAlignment lineAligment) {
             _titleFont         = titleFont;
             _detailsFont       = detailsFont;
+            _imageSize         = imageSize;
+            this.ItemHeight    = _imageSize.Height + this.Margin.Vertical;
             _fmt               = new StringFormat();
             _fmt.Alignment     = aligment;
             _fmt.LineAlignment = lineAligment;
@@ -70,6 +91,8 @@ namespace OphiussaServerManager {
 
         public ExListBox() {
             InitializeComponent();
+            _imageSize         = new Size(80, 60);
+            this.ItemHeight    = _imageSize.Height + this.Margin.Vertical;
             _fmt               = new StringFormat();
             _fmt.Alignment     = StringAlignment.Near;
             _fmt.LineAlignment = StringAlignment.Near;
@@ -82,7 +105,7 @@ namespace OphiussaServerManager {
             // prevent from error Visual Designer
             if (Items.Count > 0 && e.Index >= 0) {
                 var item = (ExListBoxItem)Items[e.Index];
-                item.DrawItem(e, Margin, _titleFont, _detailsFont, _fmt);
+                item.DrawItem(e, Margin, _titleFont, _detailsFont, _fmt, this._imageSize);
             }
         }
 
